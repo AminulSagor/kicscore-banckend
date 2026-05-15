174319a3732aa1003bc1ab8c199dd11e13e5e3d4
Habibur Rahman Masum <hrmasum99.study@gmail.com>
2026-05-16 03:35:48 +0600

    faet: Admin Panel API Integrated

--- src/modules/admin/dto/create-admin-profile.dto.ts
+++ src/modules/admin/dto/create-admin-profile.dto.ts
@@ -1,9 +1,25 @@
-import { IsOptional, IsString, IsUUID, MaxLength } from 'class-validator';
+import {
+  IsEmail,
+  IsOptional,
+  IsString,
+  IsUUID,
+  MaxLength,
+  MinLength,
+} from 'class-validator';
 
 export class CreateAdminProfileDto {
   @IsString()
   @MaxLength(80)
   fullName: string;
+
+  @IsEmail()
+  @MaxLength(120)
+  email: string;
+
+  @IsString()
+  @MinLength(8)
+  @MaxLength(80)
+  password: string;
 
   @IsOptional()
   @IsUUID()
